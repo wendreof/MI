068ad74fae974b4a694bd2041c87486fe7079dc2
DouglasDomenciano <douglas_domenciano@yahoo.com.br>
2017-04-19 16:19:13 -0300

    Clean Bugs

--- MyInner/Controller/Register.php
+++ MyInner/Controller/Register.php
@@ -7,7 +7,7 @@ session_start();
 
 
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include("ConnectionFactory.php");
     $email_user = $_POST['email_user'];
     $user_name = $_POST['user_name'];
@@ -25,22 +25,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $role_user = NULL;
 
 
-
-
     $sql_Login = "CALL SP_INSERT_LOGIN(\"$email_user\",\"$user_name\",\"$password\");";
     $sql_Register = "CALL SP_INSERT_USER(\"$name_user\",\"$sobrenome_user\",\"$endereco_user\",\"$email_user\",\"$sexo_user\",\"$cidade_user\",\"$estado_user\",\"$pais_user\",\"$datanasc_user\",\"$role_user\",\"$user_name\",\"$terms_acept\");";
 
 
-    mysqli_query($conexao,$sql_Login);
-    mysqli_query($conexao,$sql_Register);
+    mysqli_query($conexao, $sql_Login);
+    mysqli_query($conexao, $sql_Register);
     echo mysqli_error($conexao);
     header("Location: ../View/Sign_In_MyInner.php");
 
 
-
-
-
-
+}
 
 
 ?>
