4f320afa7469059ca40085326fe9c1ca7df76e6c
WLF <wendreoofernandes@gmail.com>
2017-04-25 00:39:37 -0300

    Add class Ticket and Assesment

--- MyInner/Controller/Select_User.php
+++ MyInner/Controller/Select_User.php
@@ -1,32 +1,30 @@
 <html><pre></html>
 <?php  // Exibe dados do perfil (Foto, dados e todas as avaliações anteriores)
-include 'ConnectionFactory.php';
-session_start(); //inicia a sessão
-$us = $_GET['user'];
+    include 'ConnectionFactory.php';
+        session_start(); //inicia a sessão
+        $us = $_GET['user'];
 
 
-$sql = "CALL SP_SELECT_USER ('$us');"; // SQL PARA CONSULTAR OS DADOS DO USUÁRIO
+    $sql = "CALL SP_SELECT_USER ('$us');"; // SQL PARA CONSULTAR OS DADOS DO USUÁRIO
 
-$result = mysqli_query($conexao, $sql);
+            $result = mysqli_query($conexao, $sql);
 
-while($row = mysqli_fetch_array($result)){
-        $_SESSION['ID_USER'] = $row['ID_USER'];
-        $_SESSION['NOME_USER'] = $row['NOME_USER'];
-        $_SESSION['SOBRENOME_USER'] = $row['SOBRENOME_USER'];
-        $_SESSION['ENDERECO_USER'] = $row['ENDERECO_USER'];
-        $_SESSION['EMAIL_USER'] = $row['EMAIL_USER'];
-        $_SESSION['SEXO_USER'] = $row['SEXO_USER'];
-        $_SESSION['CIDADE_USER'] = $row['CIDADE_USER'];
-        $_SESSION['ESTADO_USER'] = $row['ESTADO_USER'];
-        $_SESSION['PAIS_USER'] = $row['PAIS_USER'];
-        $_SESSION['DATANASC_USER'] = $row['DATANASC_USER'];
-        $_SESSION['USER_NAME'] = $row['USER_NAME'];
-        $_SESSION['EMAIL_USER'] = $row['EMAIL_USER'];
-        $_SESSION['ROLE_USER'] = $row['ROLE_USER'];
-
-
-}
-//header("Location: Dashboard_MyInner.php");
+    while($row = mysqli_fetch_array($result)){
+            $_SESSION['ID_USER'] = $row['ID_USER'];
+            $_SESSION['NOME_USER'] = $row['NOME_USER'];
+            $_SESSION['SOBRENOME_USER'] = $row['SOBRENOME_USER'];
+            $_SESSION['ENDERECO_USER'] = $row['ENDERECO_USER'];
+            $_SESSION['EMAIL_USER'] = $row['EMAIL_USER'];
+            $_SESSION['SEXO_USER'] = $row['SEXO_USER'];
+            $_SESSION['CIDADE_USER'] = $row['CIDADE_USER'];
+            $_SESSION['ESTADO_USER'] = $row['ESTADO_USER'];
+            $_SESSION['PAIS_USER'] = $row['PAIS_USER'];
+            $_SESSION['DATANASC_USER'] = $row['DATANASC_USER'];
+            $_SESSION['USER_NAME'] = $row['USER_NAME'];
+            $_SESSION['EMAIL_USER'] = $row['EMAIL_USER'];
+            $_SESSION['ROLE_USER'] = $row['ROLE_USER'];
+    }
+    //header("Location: Dashboard_MyInner.php");
 
 ?>
 <html></pre></html>
---
+++ MyInner/Model/Ticket.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: WLF
+ * Date: 25/04/17
+ * Time: 00:35
+ */
+class Ticket
+{
+
+}
