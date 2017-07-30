3c21a597573310d00bb7f29c2e3e9f04fed07621
WLF <wendreoofernandes@gmail.com>
2017-07-30 18:24:07 -0300

    Altering live-chat

--- MyInner/View/Header.php
+++ MyInner/View/Header.php
@@ -7,6 +7,18 @@ if ($_SESSION['valid']){
 <!DOCTYPE html>
 <html>
     <head>
+
+    
+<!--Start of Zendesk Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?4y1pudTiplEm8VTUGn0XjUTsWL52ojqu";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zendesk Chat Script-->
+
 <title>My Inner - A revolução ema avaliações.</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
